bb51de0f08c8b191b24323eb9072d23c5df926ae
Fabricio Nogueira <fabricio.nogueira@instruct.com.br>
2019-09-21 11:47:23 -0300

    feat(nav): Define top bar nav style

--- src/components/app-nav/AppNav.vue
+++ src/components/app-nav/AppNav.vue
@@ -1,25 +1,26 @@
 <template>
   <a-layout-header class="app-layout-nav">
-    <div class="app-layout-nav-logo">My To Do</div>
-    <div class="app-layout-nav-counters">
-      <a-col :span="6">
-        <span class="app-layout-nav-counters--title">To Do:</span>
-
-        <span class="app-layout-nav-counters--destaq">{{ total_of_todo.length || 0 }}</span>
-      </a-col>
-      <a-col :span="6">
-        <span class="app-layout-nav-counters--title">Done:</span>
-
-        <span class="app-layout-nav-counters--destaq">{{ total_of_done.length || 0 }}</span>
-      </a-col>
-      <a-col :span="6">
-        <span class="app-layout-nav-counters--title">Total:</span>
-
-        <span
-          class="app-layout-nav-counters--destaq"
-        >{{ (total_of_done.length + total_of_todo.length) || 0 }}</span>
-      </a-col>
-    </div>
+    <a-col :span="4">
+      <div class="app-layout-nav-logo">My To Do</div>
+    </a-col>
+    <a-col :span="20">
+      <div class="app-layout-nav-counters">
+        <a-col :span="8">
+          <span class="app-layout-nav-counters--title">To Do:</span>
+          <span class="app-layout-nav-counters--destaq">{{ total_of_todo.length || 0 }}</span>
+        </a-col>
+        <a-col :span="8">
+          <span class="app-layout-nav-counters--title">Done:</span>
+          <span class="app-layout-nav-counters--destaq">{{ total_of_done.length || 0 }}</span>
+        </a-col>
+        <a-col :span="8">
+          <span class="app-layout-nav-counters--title">Total:</span>
+          <span
+            class="app-layout-nav-counters--destaq"
+          >{{ (total_of_done.length + total_of_todo.length) || 0 }}</span>
+        </a-col>
+      </div>
+    </a-col>
   </a-layout-header>
 </template>
 
