e7395f8db89b584349e5e9f59e861954a1b8709b
Fabricio Nogueira <fabricio.nogueira@instruct.com.br>
2019-09-21 16:51:10 -0300

    feat(export): Add export data component

---
+++ src/components/app-export/AppExport.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-button
+    ghost
+    type="primary"
+    icon="file-excel"
+    :loading="loading"
+    @click="handlerExport"
+    class="btn btn-large"
+  >
+    <span v-if="loading">Generating...</span>
+    <span v-else>Export</span>
+  </a-button>
+</template>
+
+<script src="./app-export.js"></script>
---
+++ src/components/app-export/app-export.js
@@ -0,0 +1,41 @@
+export default {
+  name: 'app-export',
+  props: {
+    source: {
+      type: String,
+      required: true,
+    },
+    resource: {
+      type: Array,
+      required: true,
+    },
+  },
+  data: () => {
+    return {
+      loading: false,
+    };
+  },
+  methods: {
+    handlerExport() {
+      this.loading = true;
+      setTimeout(() => {
+        let csv = `${Object.keys(...this.resource).join(',')}\n`;
+        this.resource.forEach(item => {
+          csv += `${Object.values(item).join(',')}`;
+          csv += '\n';
+        });
+        this.loading = false;
+
+        const hidden = document.createElement('a');
+        hidden.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+        hidden.target = '_blank';
+        hidden.download = `${this.source}_${new Date()
+          .toISOString()
+          .replace(' ', '_')
+          .toLowerCase()}.csv`;
+        hidden.click();
+        this.$message.info('Success exported');
+      }, 1000);
+    },
+  },
+};
